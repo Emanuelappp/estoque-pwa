2e524aa8b4ab86cd613caffcba03803795e545c4
Emanuelappp <emanuelfernandesapp@gmail.com>
2025-04-25 11:52:15 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,41 @@
-const CACHE_NAME = "packing-cache-v1";
+const CACHE_NAME = "packing-cache-v2";
 const urlsToCache = [
-  "index.html",
-  "manifest.json",
-  "icons/icon-192.png",
-  "icons/icon-512.png"
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "/icons/icon-144.png",
+  "https://goldfruit.com.br/wp-content/uploads/2021/01/GoldFruit__1_-removebg-preview.png",
+  "https://api.sheetmonkey.io/form/9Uw9rdDSN8Se4xDun5tXYj"
 ];
 
-// Instala o Service Worker e faz cache dos arquivos
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
-// Ativa e remove caches antigos
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) return caches.delete(cache);
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
         })
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
-// Responde com os arquivos do cache (modo offline)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/index.html'))
   );
 });
